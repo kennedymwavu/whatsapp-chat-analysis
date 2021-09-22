4e9a7d8db0425a14cd65bc002911551b3b448363
KennedyMwavu <mwavukennedy@gmail.com>
2021-09-22 07:29:44 +0300

    Texts per day bars & texts per month linechart (Stats > overview)

--- server.R
+++ server.R
@@ -1,5 +1,19 @@
 server <- function(input, output, session) {
+  # Texts per day plotly:
   output$textsPerDayBars <- renderPlotly({
     textsPerDayBars
+  })
+  
+  output$textsPerDayBarsVB <- renderValueBox({
+    valueBox(
+      value = paste0(
+        dayWithMostTexts$n
+      ), 
+      subtitle = "Highest number of texts sent in a day so far"
+    )
+  })
+  
+  output$textsPerMonthLineChart <- renderPlotly({
+    textsPerMonthLineChart
   })
 }
